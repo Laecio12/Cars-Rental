5a823de13f4aa5318c68336e333d74a9d6c11532
laecio12 <laecio12silva@gmail.com>
2021-08-16 20:40:25 -0300

    adding JWT

---
+++ src/modules/accounts/useCase/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,46 @@
+import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
+import { injectable, inject } from "tsyringe";
+
+import { IUserRepository } from "../../repositories/IUserRepository";
+
+interface IRequest {
+  email: string;
+  password: string;
+}
+interface IResponse {
+  user: {
+    name: string;
+    email: string;
+  };
+  token: string;
+}
+
+@injectable()
+class AuthenticateUserUseCase {
+  constructor(
+    @inject("UserRepository")
+    private userRepository: IUserRepository
+  ) {}
+  async execute({ email, password }: IRequest): Promise<IResponse> {
+    const user = await this.userRepository.findByEmail(email);
+
+    if (user) {
+      throw new Error("Email or password incorrect.!");
+    }
+    const passwordMatch = await compare(password, user.password);
+
+    if (!passwordMatch) {
+      throw new Error("Email or password incorrect!");
+    }
+
+    const token = sign({}, "a50f311cac4781a23d6376585bace9d3a3e12453", {
+      subject: user.id,
+      expiresIn: 60 * 60 * 24, // expire in 24 hours.
+    });
+    return {
+      user,
+      token,
+    };
+  }
+}
