5af9990bdef5309e2384ba58be831cb8cb9fd61a
laecio12 <laecio12silva@gmail.com>
2021-08-20 17:27:05 -0300

    adding Authenticate user

--- src/modules/accounts/useCase/authenticateUser/AuthenticateUserUseCase.ts
+++ src/modules/accounts/useCase/authenticateUser/AuthenticateUserUseCase.ts
@@ -6,13 +6,17 @@ import { IUserRepository } from "../../repositories/IUserRepository";
 
 interface IRequest {
   email: string;
+
   password: string;
 }
+
 interface IResponse {
   user: {
     name: string;
+
     email: string;
   };
+
   token: string;
 }
 
@@ -22,12 +26,14 @@ class AuthenticateUserUseCase {
     @inject("UserRepository")
     private userRepository: IUserRepository
   ) {}
+
   async execute({ email, password }: IRequest): Promise<IResponse> {
     const user = await this.userRepository.findByEmail(email);
 
     if (user) {
       throw new Error("Email or password incorrect.!");
     }
+
     const passwordMatch = await compare(password, user.password);
 
     if (!passwordMatch) {
@@ -36,11 +42,16 @@ class AuthenticateUserUseCase {
 
     const token = sign({}, "a50f311cac4781a23d6376585bace9d3a3e12453", {
       subject: user.id,
+
       expiresIn: 60 * 60 * 24, // expire in 24 hours.
     });
+
     return {
       user,
+
       token,
     };
   }
 }
+
+export { AuthenticateUserUseCase };
